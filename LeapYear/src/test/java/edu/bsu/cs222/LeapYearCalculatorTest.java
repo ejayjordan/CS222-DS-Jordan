a02f7d92d8ddb1f061b45f55a26ce342eaf58ced
jay <emma.jordan@bsu.edu>
2023-01-17 13:34:14 -0500

    LeapYear

--- LeapYear/src/test/java/edu/bsu/cs222/LeapYearCalculatorTest.java
+++ LeapYear/src/test/java/edu/bsu/cs222/LeapYearCalculatorTest.java
@@ -1,4 +1,12 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class LeapYearCalculatorTest {
+    @Test
+    public void test2004isLeapYear(){
+        LeapYearCalculator LeapYear = new LeapYearCalculator();
+        boolean result = LeapYear.isLeapYear(2004);
+    }
 }
